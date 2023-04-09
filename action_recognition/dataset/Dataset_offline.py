6f1b621f31b5d37da5c4ac8d3d827e588c3028db
tsubasa mizuno <t.mizuno.384@nitech.jp>
2023-04-09 10:56:20 +0900

    2023030409

--- action_recognition/dataset/Dataset_offline.py
+++ action_recognition/dataset/Dataset_offline.py
@@ -86,7 +86,8 @@ class AlignedDataset(Dataset):
         # shape:H*W*3が欲しい
         # ----実画像-----
         print(image_list[rand_index])
-        image_numpy = imread(image_list[rand_index])
+        pil_image = Image.open(image_list[rand_index])
+        image_numpy = np.array(pil_image)
         image_tensor = torch.from_numpy(image_numpy).permute(2, 0, 1)
 
         # shape:H*W欲しい
